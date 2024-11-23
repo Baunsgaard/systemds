3db4f827f32068280c3609eb5ae00846fb8cfd36
Sebastian Baunsgaard <baunsgaard@apache.org>
2024-11-23 22:05:48 +0100

    comment out scuro

--- src/main/python/systemds/__init__.py
+++ src/main/python/systemds/__init__.py
@@ -22,6 +22,6 @@
 from systemds import context
 from systemds import operator
 from systemds import examples
-from systemds import scuro
+# from systemds import scuro
 
 __all__ = ["context", "operator", "examples", "scuro"]
